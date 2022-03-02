a25c98ea4a5d077b7c5a64678c5b7a6d69df4a28
KilianHnidek <kilian.hnidek@stud.fh-campuswien.ac.at>
2022-03-02 13:29:03 +0100

    Import jetzt richtig

--- src/test/java/at/ac/fhcampuswien/AppTest.java
+++ src/test/java/at/ac/fhcampuswien/AppTest.java
@@ -1,10 +1,7 @@
 package at.ac.fhcampuswien;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.List;
