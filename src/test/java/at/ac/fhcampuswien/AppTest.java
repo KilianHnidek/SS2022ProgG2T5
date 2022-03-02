1c10f7cf4e2d82111f6f46555d3c049d36568a14
derklausberger <niklasklausberger@gmail.com>
2022-03-02 13:06:39 +0100

    add BeforeAll and AfterAll to AppTest

--- src/test/java/at/ac/fhcampuswien/AppTest.java
+++ src/test/java/at/ac/fhcampuswien/AppTest.java
@@ -1,4 +1,23 @@
 package at.ac.fhcampuswien;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class AppTest {
+
+    @BeforeAll
+    public static void init() {
+        System.out.println("Testing Exercise 1");
+    }
+
+    @AfterAll
+    public static void finish() {
+        System.out.println("Finished Testing Exercise 1");
+    }
 }
