6c6c422cfed4ca97e4e26b89ffd158849cb5c2bf
derklausberger <niklasklausberger@gmail.com>
2022-03-03 20:49:51 +0100

    add needed tests for methods (done by all group members)

--- src/test/java/at/ac/fhcampuswien/AppTest.java
+++ src/test/java/at/ac/fhcampuswien/AppTest.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AppTest {
-
     @BeforeAll
     public static void init() {
         System.out.println("Testing Exercise 1");
@@ -103,7 +102,137 @@ public class AppTest {
 
             assertEquals(articles.size(), controller.getArticleCount());
             assertEquals(3, controller.getArticleCount());
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Ups something went terribly wrong here...");
+        }
+    }
 
+    @Test
+    public void testGetTopHeadlinesAustria1() {
+        // Checks for modifier and return type
+        try {
+            Method getArticleCount = AppController.class.getDeclaredMethod("getTopHeadlinesAustria");
+            assertEquals("public", Modifier.toString(getArticleCount.getModifiers()), "getTopHeadlinesAustria");
+            assertEquals("interface java.util.List", getArticleCount.getReturnType().toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Ups something went terribly wrong here...");
+        }
+    }
+
+    @Test
+    public void testGetTopHeadlinesAustria2() {
+        try {
+            AppController controller = new AppController();
+            assertTrue(controller.getTopHeadlinesAustria().size() == 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Ups something went terribly wrong here...");
+        }
+    }
+
+    // following test might be deleted/updated later
+
+    @Test
+    public void testGetTopHeadlinesAustria3() {
+        // Setting
+        try {
+            List<Article> articles = new ArrayList<>();
+            AppController controller = new AppController();
+
+            articles.add(new Article("Papa Putin", "Aide Ukraine"));
+            articles.add(new Article("Jeff Bezos", "Nagelneuer Benzer"));
+            articles.add(new Article("Mama", "Mach Wäsche"));
+            controller.setArticles(articles);
+
+            assertEquals(3, articles.size());
+            assertEquals(articles.size(), controller.getTopHeadlinesAustria().size());
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Ups something went terribly wrong here...");
+        }
+    }
+
+    // insert more tests for getTopHeadlinesAustria here
+
+    @Test
+    public void testGetAllNewsBitcoin1() {
+        // Checks for modifier and return type
+        try {
+            Method getArticleCount = AppController.class.getDeclaredMethod("getAllNewsBitcoin");
+            assertEquals("public", Modifier.toString(getArticleCount.getModifiers()), "getAllNewsBitcoin");
+            assertEquals("interface java.util.List", getArticleCount.getReturnType().toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Ups something went terribly wrong here...");
+        }
+    }
+
+    @Test
+    public void testGetAllNewsBitcoin2() {
+        try {
+            AppController controller = new AppController();
+            List<Article> articles = new ArrayList<>();
+
+            articles.add(new Article("abc", "BITCOIN"));
+            articles.add(new Article("abc", "hallo bitcoin"));
+            articles.add(new Article("abc", "BiTcOiN"));
+            articles.add(new Article("abc", "gönnBitcoinHallo"));
+            articles.add(new Article("abc", "hallo"));
+            articles.add(new Article("abc", "nichtkrypto"));
+
+            controller.setArticles(articles);
+            assertEquals(controller.getAllNewsBitcoin().size(), 4);
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Ups something went terribly wrong here...");
+        }
+    }
+
+    @Test
+    public void testGetAllNewsBitcoin3() {
+        try {
+            AppController controller = new AppController();
+            List<Article> articles = new ArrayList<>();
+
+            articles.add(new Article("abc", "BTC"));
+
+            controller.setArticles(articles);
+            assertEquals(controller.getAllNewsBitcoin().size(), 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Ups something went terribly wrong here...");
+        }
+    }
+
+    @Test
+    public void testFilterList1() {
+        // Checks for modifier and return type
+        try {
+            Method getArticleCount = AppController.class.getDeclaredMethod("filterList", String.class, List.class);
+            assertEquals("protected static", Modifier.toString(getArticleCount.getModifiers()), "filterList");
+            assertEquals("interface java.util.List", getArticleCount.getReturnType().toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Ups something went terribly wrong here...");
+        }
+    }
+
+    @Test
+    public void testFilterList2() {
+        try {
+            List<Article> articles = new ArrayList<>();
+
+            articles.add(new Article("abc", "BItcoin"));
+            articles.add(new Article("abc", "hallo bitcoin"));
+            articles.add(new Article("abc", "BiTcOiN"));
+            articles.add(new Article("abc", "gönnBitcoinHallo"));
+            articles.add(new Article("abc", "hallo"));
+            articles.add(new Article("abc", "nichtkrypto"));
+
+            assertEquals(AppController.filterList("Co", articles).size(), 4);
+            assertEquals(AppController.filterList("x", articles).size(), 0);
         } catch (Exception e) {
             e.printStackTrace();
             fail("Ups something went terribly wrong here...");
