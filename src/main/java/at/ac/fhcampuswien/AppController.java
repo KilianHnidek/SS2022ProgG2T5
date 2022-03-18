54eaae6836577bb8a594b4387bfe6b13e9808941
KilianHnidek <kilian.hnidek@stud.fh-campuswien.ac.at>
2022-03-18 22:11:33 +0100

    FX getTopHeadlinesAustria displayed via two VBoxes -by all members :)

--- src/main/java/at/ac/fhcampuswien/AppController.java
+++ src/main/java/at/ac/fhcampuswien/AppController.java
@@ -47,12 +47,25 @@ public class AppController {
     private static List<Article> generateMockList() {
 
         List<Article> articles = new ArrayList<>();
+
         articles.add(new Article("abc", "BItcOin"));
         articles.add(new Article("abc", "hallo bitcoin"));
         articles.add(new Article("abc", "BiTcOiN"));
         articles.add(new Article("abc", "gibMalBitcoinHallo"));
         articles.add(new Article("abc", "hallo"));
         articles.add(new Article("abc", "nichtCrypto"));
+
+        articles.add(new Article("George Orwell", "21984"));
+        articles.add(new Article("hallo", "2BItcOin"));
+        articles.add(new Article("abc", "2hal2lo bitcoin"));
+        articles.add(new Article("abc", "2BiTc2OiN"));
+        articles.add(new Article("abc", "2gibMa2lBitcoinHallo"));
+        articles.add(new Article("abc", "2hallo2"));
+
+        articles.add(new Article("abc", "3BItcOin"));
+        articles.add(new Article("abc", "3hallo bitcoin"));
+        articles.add(new Article("George Orwell", "31984"));
+
         return articles;
     }
 }
--- src/main/java/at/ac/fhcampuswien/controller/MenuController.java
+++ src/main/java/at/ac/fhcampuswien/controller/MenuController.java
@@ -2,34 +2,43 @@ package at.ac.fhcampuswien.controller;
 
 import at.ac.fhcampuswien.AppController;
 import at.ac.fhcampuswien.Article;
-import at.ac.fhcampuswien.Menu;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.input.MouseDragEvent;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.robot.Robot;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.util.List;
 
 public class MenuController {
+
     private AppController ctrl = new AppController();
 
-    @FXML
-    private Pane emptyPane;
+    private double mouseX;
+    private int articleCounter = 12;
 
     @FXML
-    private Label allnewsaboutbitcoin, countarticles, quitprogram, topheadlinesaustria;
+    private Pane emptyPane, anchorPane;
+    @FXML
+    private VBox vBoxArticlesLeft, vBoxArticlesRight;
+    @FXML
+    private Label allNewsAboutBitcoin, countArticles, quitProgram, topHeadlinesAustria;
 
     @FXML
-    void handleMouseEntered(MouseEvent event) {
+    void handleMouseDragged(MouseEvent event) {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setX(event.getScreenX() - stage.getWidth() / 2);
+        stage.setX(event.getScreenX() - mouseX);
         stage.setY(event.getScreenY() - emptyPane.getHeight() / 2);
+    }
+
+    @FXML
+    void handleMousePressed(MouseEvent event) {
+        mouseX = event.getSceneX();
     }
 
     @FXML
@@ -44,7 +53,50 @@ public class MenuController {
 
     @FXML
     void getTopHeadlinesAustria(MouseEvent event) {
-        ctrl.getTopHeadlinesAustria();
+        for (Node n : anchorPane.getChildren()) {
+            if (n.getClass().getSimpleName().equals("Label")) {
+                n.setDisable(true);
+                n.setVisible(false);
+            }
+        }
+
+        int counterLeftRight = 0;
+
+        List<Article> articles = ctrl.getTopHeadlinesAustria();
+
+        for (int k = articleCounter; k < articleCounter + 6; k++) {
+
+            if (articleCounter < articles.size()) {
+                Label l1 = new Label();
+                l1.alignmentProperty().set(Pos.CENTER);
+                l1.setText(articles.get(articleCounter).getTitle());
+                l1.setFont(Font.font("Times New Roman", 16));
+                l1.setPadding(new Insets(0, 0, 5, 0)); // top, right, bottom, left
+
+                Label l2 = new Label();
+                l2.alignmentProperty().set(Pos.CENTER);
+                l2.setText(articles.get(articleCounter).getAuthor());
+                l2.setFont(Font.font("Times New Roman", 12));
+                l2.setPadding(new Insets(0, 0, 20, 0)); // top, right, bottom, left
+
+
+                if (counterLeftRight < 3) {
+                    vBoxArticlesLeft.getChildren().add(l1);
+                    vBoxArticlesLeft.getChildren().add(l2);
+                } else {
+                    vBoxArticlesRight.getChildren().add(l1);
+                    vBoxArticlesRight.getChildren().add(l2);
+                }
+                counterLeftRight++;
+                articleCounter++;
+
+                if (counterLeftRight % 6 == 0) k = articleCounter + 50000;
+            } else {
+                k = articleCounter + 50000;
+            }
+        }
+            counterLeftRight = 0;
+
     }
 
     @FXML
